1886421f05ee2157ebc4fee42d15946ea39dd5ef
PC-GITHUB-Account <pankajchandravanshi.iitm@gmail.com>
2021-01-14 06:39:30 +0530

    Done with Generic Code

---
+++ Foreign-Exchange-Rates/src/main/java/pojo/Rates.java
@@ -0,0 +1,231 @@
+package pojo;
+
+public class Rates {
+	
+	private String GBP;
+	private String  HKD;
+	private String IDR;
+	private String ILS;
+	private String DKK;
+	private String INR;
+	private String CHF;
+	private String MXN;
+	private String CZK;
+	private String SGD;
+	private String THB;
+	private String HRK;
+	private String MYR;
+	private String NOK;
+	private String CNY;
+	private String BGN;
+	private String PHP;
+	private String SEK;
+	private String PLN;
+	private String ZAR;
+	private String CAD;
+	private String ISK;
+	private String BRL;
+	private String RON;
+	private String NZD;
+	private String TRY;
+	private String JPY;
+	private String RUB;
+	private String KRW;
+	private String USD;
+	private String HUF;
+	private String AUD;
+	
+	public String getGBP() {
+		return GBP;
+	}
+	public void setGBP(String gBP) {
+		GBP = gBP;
+	}
+	public String getHKD() {
+		return HKD;
+	}
+	public void setHKD(String hKD) {
+		HKD = hKD;
+	}
+	public String getIDR() {
+		return IDR;
+	}
+	public void setIDR(String iDR) {
+		IDR = iDR;
+	}
+	public String getILS() {
+		return ILS;
+	}
+	public void setILS(String iLS) {
+		ILS = iLS;
+	}
+	public String getDKK() {
+		return DKK;
+	}
+	public void setDKK(String dKK) {
+		DKK = dKK;
+	}
+	public String getINR() {
+		return INR;
+	}
+	public void setINR(String iNR) {
+		INR = iNR;
+	}
+	public String getCHF() {
+		return CHF;
+	}
+	public void setCHF(String cHF) {
+		CHF = cHF;
+	}
+	public String getMXN() {
+		return MXN;
+	}
+	public void setMXN(String mXN) {
+		MXN = mXN;
+	}
+	public String getCZK() {
+		return CZK;
+	}
+	public void setCZK(String cZK) {
+		CZK = cZK;
+	}
+	public String getSGD() {
+		return SGD;
+	}
+	public void setSGD(String sGD) {
+		SGD = sGD;
+	}
+	public String getTHB() {
+		return THB;
+	}
+	public void setTHB(String tHB) {
+		THB = tHB;
+	}
+	public String getHRK() {
+		return HRK;
+	}
+	public void setHRK(String hRK) {
+		HRK = hRK;
+	}
+	public String getMYR() {
+		return MYR;
+	}
+	public void setMYR(String mYR) {
+		MYR = mYR;
+	}
+	public String getNOK() {
+		return NOK;
+	}
+	public void setNOK(String nOK) {
+		NOK = nOK;
+	}
+	public String getCNY() {
+		return CNY;
+	}
+	public void setCNY(String cNY) {
+		CNY = cNY;
+	}
+	public String getBGN() {
+		return BGN;
+	}
+	public void setBGN(String bGN) {
+		BGN = bGN;
+	}
+	public String getPHP() {
+		return PHP;
+	}
+	public void setPHP(String pHP) {
+		PHP = pHP;
+	}
+	public String getSEK() {
+		return SEK;
+	}
+	public void setSEK(String sEK) {
+		SEK = sEK;
+	}
+	public String getPLN() {
+		return PLN;
+	}
+	public void setPLN(String pLN) {
+		PLN = pLN;
+	}
+	public String getZAR() {
+		return ZAR;
+	}
+	public void setZAR(String zAR) {
+		ZAR = zAR;
+	}
+	public String getCAD() {
+		return CAD;
+	}
+	public void setCAD(String cAD) {
+		CAD = cAD;
+	}
+	public String getISK() {
+		return ISK;
+	}
+	public void setISK(String iSK) {
+		ISK = iSK;
+	}
+	public String getBRL() {
+		return BRL;
+	}
+	public void setBRL(String bRL) {
+		BRL = bRL;
+	}
+	public String getRON() {
+		return RON;
+	}
+	public void setRON(String rON) {
+		RON = rON;
+	}
+	public String getNZD() {
+		return NZD;
+	}
+	public void setNZD(String nZD) {
+		NZD = nZD;
+	}
+	public String getTRY() {
+		return TRY;
+	}
+	public void setTRY(String tRY) {
+		TRY = tRY;
+	}
+	public String getJPY() {
+		return JPY;
+	}
+	public void setJPY(String jPY) {
+		JPY = jPY;
+	}
+	public String getRUB() {
+		return RUB;
+	}
+	public void setRUB(String rUB) {
+		RUB = rUB;
+	}
+	public String getKRW() {
+		return KRW;
+	}
+	public void setKRW(String kRW) {
+		KRW = kRW;
+	}
+	public String getUSD() {
+		return USD;
+	}
+	public void setUSD(String uSD) {
+		USD = uSD;
+	}
+	public String getHUF() {
+		return HUF;
+	}
+	public void setHUF(String hUF) {
+		HUF = hUF;
+	}
+	public String getAUD() {
+		return AUD;
+	}
+	public void setAUD(String aUD) {
+		AUD = aUD;
+	}
+
+}
---
+++ Foreign-Exchange-Rates/src/test/java/payloads/CurrExchangeRatePayloads.java
@@ -0,0 +1,5 @@
+package payloads;
+
+public class CurrExchangeRatePayloads {
+
+}
